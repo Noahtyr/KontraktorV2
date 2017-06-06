e277b30443c8df76d2f363cccdde15c9dfa03eef
Sakref00unholy <phillipmayland@gmail.com>
2017-06-06 11:10:17 +0200

    aktivitet 4 færdig

---
+++ 4.saerligeydelser/app/src/main/java/phillipmayland/a4saerligeydelser/aktivitet4.java
@@ -0,0 +1,13 @@
+package phillipmayland.a4saerligeydelser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aktivitet4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktivitet4);
+    }
+}
